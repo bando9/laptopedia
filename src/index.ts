dc78ab26019bf36495dc8af99e73c8ddab80366e
bando9 <bandomega1@gmail.com>
2025-12-08 21:10:56 +0700

    refactor: move post, delete, delete all, patch to modules/laptops/routes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { laptopRoutes } from "./modules/laptops/routes";
-import { dataLaptops } from "./data";
 import { commonRoute } from "./modules/common/route";
 
 const app = new Hono();
@@ -10,46 +9,5 @@ app.use(logger());
 
 app.route("/", commonRoute);
 app.route("/laptops", laptopRoutes);
-
-app.post("/laptops", (c) => {
-  return c.json({
-    id: "lap_006",
-    brand: "Asus",
-    model: "Asus Vivobook 14",
-  });
-});
-
-app.delete("/laptops/:id", (c) => {
-  const laptopId = c.req.param("id");
-  const updateData = dataLaptops.filter((laptop) => laptop.id !== laptopId);
-
-  return c.json(updateData);
-});
-
-app.delete("/laptops", (c) => {
-  let data = dataLaptops;
-  const updateData = (data = []);
-  return c.json(updateData);
-});
-
-app.put("/laptops/:slug", (c) => {
-  const laptopSlug = c.req.param("slug");
-
-  const updateData = dataLaptops.map((laptop) => {
-    if (laptop.slug === laptopSlug) {
-      const updateLaptop = {
-        ...laptop,
-        cpu: "AMD Ryzen 7 7840HS",
-        gpu: "NVIDIA GeForce RTX 4060",
-        ram: "16GB DDR5",
-        storage: "512GB SSD",
-      };
-      return updateLaptop;
-    }
-    return laptop;
-  });
-
-  return c.json(updateData);
-});
 
 export default app;
--- src/modules/laptops/routes.ts
+++ src/modules/laptops/routes.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
-import { dataLaptops } from "../../data";
+import { dataLaptops, dataLaptops as initialData } from "../../data";
+import { Laptop } from "../../types/laptops";
 
 export const laptopRoutes = new Hono();
 
@@ -17,4 +18,58 @@ laptopRoutes.get("/:slug", (c) => {
   }
 
   return c.json(foundLaptop);
+});
+
+laptopRoutes.post("/", (c) => {
+  const newDataLaptop: Laptop = {
+    id: "lap_006",
+    brand: "Asus",
+    model: "Asus Vivobook 14",
+    slug: "asus-vivobook-14",
+    cpu: "",
+    gpu: "",
+    ram: "8GB DDR4",
+    storage: "512GB SSD",
+    display: "",
+    battery: "",
+    weight: "",
+    release_year: 2022,
+    price: 8500000,
+  };
+
+  const updatedData = [...dataLaptops, newDataLaptop];
+
+  return c.json(updatedData);
+});
+
+laptopRoutes.delete("/:id", (c) => {
+  const laptopId = c.req.param("id");
+  const updatedData = dataLaptops.filter((laptop) => laptop.id !== laptopId);
+  return c.json(updatedData);
+});
+
+laptopRoutes.delete("/", (c) => {
+  let laptops = [...initialData];
+  laptops = [];
+  return c.json(laptops);
+});
+
+laptopRoutes.patch("/:slug", (c) => {
+  const laptopSlug = c.req.param("slug");
+
+  const updatedData = dataLaptops.map((laptop) => {
+    if (laptop.slug === laptopSlug) {
+      const updatedLaptop = {
+        ...laptop,
+        cpu: "AMD Ryzen 7 7840HS",
+        gpu: "NVIDIA GeForce RTX 4060",
+        ram: "16GB DDR5",
+        storage: "512GB SSD",
+      };
+      return updatedLaptop;
+    }
+    return laptop;
+  });
+
+  return c.json(updatedData);
 });
