defa82662e5fe8f803064dfad51f5baf54642b41
bando9 <bandomega1@gmail.com>
2026-01-29 10:55:40 +0700

    fix: seed db logic

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,34 +3,31 @@ import { brands, initialDataLaptops } from "../src/modules/laptops/data";
 
 async function main() {
   console.log("Seeding process...");
-  const createBrands = await Promise.all(
-    brands.map((b) => prisma.brand.create({ data: b })),
-  );
 
-  const brandMap = Object.fromEntries(
-    createBrands.map((brand) => {
-      console.log(`🏷️  Brand: ${brand.slug}`);
-      return [brand.name, brand.id];
-    }),
-  );
+  for (const brand of brands) {
+    const upsertedBrand = await prisma.brand.upsert({
+      where: { slug: brand.slug },
+      create: brand,
+      update: brand,
+    });
+    console.log(`🏷️  Brand: ${upsertedBrand.slug}`);
+  }
 
-  await Promise.all(
-    initialDataLaptops.map((laptop) => {
-      const { brandSlug: brand, ...dataLaptop } = laptop;
-      console.log(`💻 Laptop: ${laptop.slug}`);
-      return prisma.laptop.create({
-        data: {
-          ...dataLaptop,
-          brandId: brandMap[brand],
-        },
-      });
-    }),
-  );
+  for (const dataLaptop of initialDataLaptops) {
+    const { brandSlug, ...laptop } = dataLaptop;
+
+    const upsetedLaptop = await prisma.laptop.upsert({
+      where: { slug: dataLaptop.slug },
+      update: { ...laptop, brand: { connect: { slug: brandSlug } } },
+      create: { ...laptop, brand: { connect: { slug: brandSlug } } },
+    });
+    console.log(`💻 Laptop: ${upsetedLaptop.slug}`);
+  }
 }
 
 main()
   .then(async () => {
-    await prisma.$connect();
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
     console.error(e);
